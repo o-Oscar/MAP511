a263b5e6c42dd9359196a422da68a8fb03a5cc34
Oscar <oscar@boutco.com>
2021-10-16 15:06:04 +0200

    Plots of the error for each quantile is working.

--- L2_error.py
+++ L2_error.py
@@ -11,11 +11,61 @@ import scipy.special
 
 import polynomial
 
+import matplotlib.pyplot as plt
+
+def main ():
+	# print_coefs()
+	# plot_all_l2 ()
+
+	debug = True
+	plot_jacobi_comp(debug=debug)
+	plot_cops_comp (debug=debug)
+
+
+def plot_q_comp (all_ja, legend="", N=-1, debug=False):
+	N = N if N > 0 else (10 if debug else 40)
+	all_q = np.linspace(0.05, 0.95, 20)
+
+	n_sample = int(1e4) if debug else int(1e5)
+	all_l2 = np.stack([ja.calc_all_l2(N, ja.c_quantile(all_q), size=n_sample) for ja in all_ja], axis=0)
+
+	plt.plot(all_q, all_l2.T, "--o")
+	plt.legend(legend)
+	plt.xlabel("Quantile q")
+	plt.ylabel("L2 Error")
+	plt.title("N="+str(N))
+	plt.show()
+
+
+def plot_jacobi_comp (debug=False):
+	all_ja = [
+		polynomial.JacobiApproximation (0, 0),
+		polynomial.JacobiApproximation (-.5, -.5),
+		polynomial.JacobiApproximation (10, 12),
+		polynomial.JacobiApproximation (-.7, .3),
+
+	]
+	plot_q_comp(all_ja, [ja.full_description() for ja in all_ja], debug=debug)
+
+def plot_cops_comp (debug=False):
+	all_ja = [
+		polynomial.HermiteApproximation (),
+		polynomial.LegendreApproximation(),
+		polynomial.JacobiApproximation (-.5, -.5),
+		polynomial.JacobiApproximation (10, 12),
+		polynomial.LaguerreApproximation(0),
+		polynomial.LaguerreApproximation(3),
+	]
+	plot_q_comp(all_ja, [ja.full_description() for ja in all_ja], debug=debug)
+
+
+
+
 
 def print_coefs ():
 	"""Prints the coefficient of the polynomials for some parameters using two means of calculation"""
 	c = .3
-	N = 4
+	N = 6
 
 	print("Hermit coefs (direct and recursive):")
 	he_app = polynomial.HermiteApproximation ()
@@ -36,10 +86,21 @@ def print_coefs ():
 	print([ja_app.gamma(n, c, use_rec=True) for n in range(N)])
 
 
+def plot_l2 (poly_app):
+	N = 20
+	all_n = list(range(1, N))
+	all_l2 = poly_app.calc_l2(N, 0.3)[1:]
+	plt.semilogy(np.log10(all_n), all_l2)
+	# plt.loglog(all_n, all_l2)
+	plt.show()
+
+def plot_all_l2 ():
+	plot_l2(polynomial.HermiteApproximation ())
+	plot_l2(polynomial.LaguerreApproximation (0))
+	plot_l2(polynomial.JacobiApproximation (3, 4))
+	plot_l2(polynomial.LegendreApproximation ())
 
 
-def main ():
-	print_coefs()
 
 if __name__ == "__main__":
 	main()
--- polynomial.py
+++ polynomial.py
@@ -9,6 +9,8 @@ class PolynomialApproximation:
 	def __init__ (self):
 		pass
 		self.all_gamma = {}
+		self.name = "Undefinded"
+		self.string_params = []
 
 	def gamma (self, n, c, use_rec=False):
 		"""Return \gamma_n(c), the nth coefficient for the approximation of 1_{X>c}"""
@@ -23,10 +25,56 @@ class PolynomialApproximation:
 	def calc_gamma_rec (self, n, c):
 		raise NotImplementedError("calc_gamma_rec has to be implemented by any subclass of PolynomialApproximation")
 
+	def poly_func (self, n, c):
+		raise NotImplementedError("poly_func has to be implemented by any subclass of PolynomialApproximation")
+
+	def random_sample (self, size):
+		raise NotImplementedError("random_sample has to be implemented by any subclass of PolynomialApproximation")
+
+	def c_quantile (self, q):
+		raise NotImplementedError("c_quantile has to be implemented by any subclass of PolynomialApproximation")
+
+	def bound_inv (self, N, q):
+		raise NotImplementedError("bound_inv has to be implemented by any subclass of PolynomialApproximation")
+
+	def calc_l2 (self, N, c, size=int(1e5)):
+		X = self.random_sample(size)
+		fX = np.greater(X, c).astype(np.float32)
+		
+		all_poly_func = [self.poly_func(n, c) for n in range(N)]
+		coefs = np.array([self.gamma(n, c, use_rec=False) for n in range(N)])
+		monoX = np.stack([poly(X) * coef for poly, coef in zip(all_poly_func, coefs)], axis=0)
+		pX = np.cumsum(monoX, axis=0)
+
+		return np.sqrt(np.mean(np.square(fX-pX), axis=1))
+	
+	def calc_all_l2 (self, N, all_c, size=int(1e5)):
+
+		X = self.random_sample(size) # shape (sample)
+		fX = np.stack([np.greater(X, c).astype(np.float32) for c in all_c], axis=0) # shape (all_c, sample)
+		
+		all_mono = [self.poly_func(n) for n in range(N)] # shape (degree)
+		coefs = np.array([[self.gamma(n, c, use_rec=False) for n in range(N)] for c in all_c]) # shape (all_c, degree)
+		monoX = np.stack([poly(X) for poly in all_mono]) # shape (degree, sample)
+		
+		# TODO : give the option to keep all intermediary polynomials in order to get the evolution of accuracy with rising degree
+		pX = coefs @ monoX # shape (all_c, sample)
+		
+		return np.sqrt(np.mean(np.square(fX-pX), axis=1)) # shape (all_c)
+	
+	def params_description (self):
+		return "   ".join(self.string_params)
+
+	def full_description (self):
+		return self.name + "   " + self.params_description()
+
 class HermiteApproximation (PolynomialApproximation):
 	def __init__ (self):
 		super().__init__()
 		self.norm = scipy.stats.norm
+
+		self.name = "Hermite"
+		self.string_params = []
 	
 	def hermite (self, n, c):
 		he = np.polynomial.hermite.Hermite(np.eye(n+1)[n])
@@ -44,11 +92,26 @@ class HermiteApproximation (PolynomialApproximation):
 		n = n2-2
 		return c / (n+2) * self.gamma(n+1, c) - n/(n+1)/(n+2) * self.gamma(n, c)
 
+	def poly_func (self, n):
+		poly = np.polynomial.hermite.Hermite(np.eye(n+1)[n])
+		return lambda X: poly(X/np.sqrt(2)) / np.power(2, n/2)
+	
+	def random_sample (self, size):
+		return np.random.normal(loc=0.0, scale=1.0, size=size)
+
+	def c_quantile (self, q):
+		return self.norm.ppf(q)
+
+	def bound_inv (self, N, q):
+		return np.exp(self.c_quantile(q)**2/4) * np.power(N, 1/4)
 
 class LaguerreApproximation (PolynomialApproximation):
 	def __init__ (self, alpha):
 		super().__init__()
 		self.alpha = alpha
+
+		self.name = "Laguerre"
+		self.string_params = ["a=" + str(alpha)]
 
 	def calc_gamma (self, n, c):
 		if n == 0:
@@ -63,11 +126,26 @@ class LaguerreApproximation (PolynomialApproximation):
 		n = n2-2
 		return (2*n+self.alpha+2-c) / (n+self.alpha+2) * self.gamma(n+1, c) - n/(n+self.alpha+2) * self.gamma(n, c)
 
+	def poly_func (self, n):
+		return scipy.special.genlaguerre(n, self.alpha)
+	
+	def random_sample (self, size):
+		return np.random.gamma(self.alpha+1, 1, size=size)
+
+	def c_quantile (self, q):
+		return scipy.stats.gamma.ppf(q, self.alpha+1)
+		
+	def bound_inv (self, N, q):
+		return np.power(self.c_quantile(q), -self.alpha/2-1/4) * np.exp(self.c_quantile(q)/2) * np.power(N, 1/4)
+
 class JacobiApproximation (PolynomialApproximation):
 	def __init__ (self, alpha, beta):
 		super().__init__()
 		self.alpha = alpha
 		self.beta = beta
+
+		self.name = "Jacobi"
+		self.string_params = ["a=" + str(alpha), "b=" + str(beta)]
 
 	def inc_beta (self, a, b, x):
 		return scipy.special.betainc(a, b, x) * scipy.special.beta(a, b)
@@ -97,3 +175,28 @@ class JacobiApproximation (PolynomialApproximation):
 	def calc_gamma_rec (self, n2, c):
 		n = n2-2
 		return self.D(n, c) * self.gamma(n+1, c) + self.E(n, c) * self.gamma(n, c)
+
+	def poly_func (self, n):
+		return scipy.special.jacobi(n, self.alpha, self.beta)
+	
+	def random_sample (self, size):
+		return 1 - 2*np.random.beta(self.alpha+1, self.beta+1, size=size)
+
+	def c_quantile (self, q):
+		return 2*scipy.stats.beta.ppf(q, self.alpha+1, self.beta+1) - 1
+
+	def bound_inv (self, N, q):
+		return np.power((1-self.c_quantile(q)), -self.alpha/2-1/4) * np.power((1+self.c_quantile(q)), -self.beta/2-1/4) * np.power(N, 1/2)
+
+class LegendreApproximation (JacobiApproximation):
+	def __init__ (self):
+		super().__init__(0, 0)
+
+		self.name = "Legendre"
+		self.string_params = []
+
+	def c_quantile (self, q):
+		return 2*q-1
+
+	def bound_inv (self, N, q):
+		return np.power((1-self.c_quantile(q)**2), -1/4) * np.power(N, 1/2)
