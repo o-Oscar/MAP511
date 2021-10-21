eedb2832c2bfeb9e86a3f798c87d0f11e3ad364b
Oscar <oscar@boutco.com>
2021-10-21 12:23:55 +0200

    Update L2_error.py to fit the current coding flavor. L2_error gives all plots.

--- python/L2_error.py
+++ python/L2_error.py
@@ -13,6 +13,31 @@ import polynomial
 
 import matplotlib.pyplot as plt
 
+from dataclasses import dataclass
+from pathlib import Path
+from os import path
+
+@dataclass
+class Config:
+	debug: bool
+	show_plots: bool
+	save_plots: bool
+
+def result_name (config):
+	res_name = "results_debug" if config.debug else "results"
+	return res_name
+
+def show_or_plot (path_suffix, config):
+	if config.save_plots:
+		res_name = result_name(config)
+		full_path = path.join(res_name, path_suffix)
+		Path(full_path).parent.mkdir(parents=True, exist_ok=True)
+		plt.savefig(full_path, dpi=300)
+	
+	if config.show_plots:
+		plt.show()
+	else:
+		plt.clf()
 
 
 def print_coefs ():
@@ -39,7 +64,7 @@ def print_coefs ():
 	print([ja_app.gamma(n, c, use_rec=True) for n in range(N)])
 
 
-def plot_l2 (poly_app, all_c, all_slopes):
+def plot_l2 (poly_app, all_c, all_slopes, config):
 	N = 80
 	all_n = list(range(1, N))
 
@@ -60,36 +85,22 @@ def plot_l2 (poly_app, all_c, all_slopes):
 
 	plt.legend(["c="+str(c) for c in all_c] + ["slope="+str(slope) for slope in all_slopes])
 	plt.title("L2 Error - " + poly_app.full_description())
-	plt.show()
+	show_or_plot(path.join("chaos_expansions", "L2_"+poly_app.name), config)
 
-def plot_all_l2 ():
-	plot_l2(polynomial.HermiteApproximation (), [0.1, 1, 2], [-1/4])
-	plot_l2(polynomial.LegendreApproximation (), [0., 0.4, 0.9], [-1/2])
-	plot_l2(polynomial.LaguerreApproximation (0), [1, 2, 3], [-1/4])
-	plot_l2(polynomial.LaguerreApproximation (3), [1, 2, 3], [-1/4])
-	plot_l2(polynomial.JacobiApproximation (3, 4), [0., 0.4, 0.6], [-1/4, -1/2])
-	plot_l2(polynomial.JacobiApproximation (12, 11), [0., 0.4, 0.6], [-1/4, -1/2])
+def plot_all_l2 (config):
+	plot_l2(polynomial.HermiteApproximation (), [0.1, 1, 2], [-1/4], config)
+	plot_l2(polynomial.LegendreApproximation (), [0., 0.4, 0.9], [-1/2], config)
+	plot_l2(polynomial.LaguerreApproximation (0), [1, 2, 3], [-1/4], config)
+	plot_l2(polynomial.LaguerreApproximation (3), [1, 2, 3], [-1/4], config)
+	plot_l2(polynomial.JacobiApproximation (3, 4), [0., 0.4, 0.6], [-1/4, -1/2], config)
+	plot_l2(polynomial.JacobiApproximation (12, 11), [0., 0.4, 0.6], [-1/4, -1/2], config)
 
-def plot_q_comp (all_ja, legend="", N=-1, debug=False):
-	N = N if N > 0 else (10 if debug else 40)
+
+def plot_jacobi_comp (config):
+	N = 10 if config.debug else 40
 	all_q = np.linspace(0.05, 0.95, 20)
 
-	n_sample = int(1e4) if debug else int(1e5)
-	all_l2 = np.stack([ja.calc_all_l2(N, ja.c_quantile(all_q), size=n_sample) for ja in all_ja], axis=0)
-
-	plt.plot(all_q, all_l2.T, "--o")
-	plt.legend(legend)
-	plt.xlabel("Quantile q")
-	plt.ylabel("L2 Error")
-	plt.title("N="+str(N))
-	plt.show()
-
-
-def plot_jacobi_comp (debug=False):
-	N = 10 if debug else 40
-	all_q = np.linspace(0.05, 0.95, 20)
-
-	n_sample = int(1e4) if debug else int(1e5)
+	n_sample = int(1e4) if config.debug else int(1e5)
 
 	all_ja = [
 		polynomial.JacobiApproximation (0, 0),
@@ -105,13 +116,12 @@ def plot_jacobi_comp (debug=False):
 	plt.xlabel("Quantile q")
 	plt.ylabel("L2 Error")
 	plt.title("N="+str(N))
-	plt.show()
+	show_or_plot(path.join("chaos_expansions", "jacobi_error"), config)
 
-def plot_cops_comp (debug=False):
-	N = 10 if debug else 50
+def plot_cops_comp (N, config):
 	all_q = np.linspace(0.05, 0.95, 20)
 
-	n_sample = int(1e4) if debug else int(1e5)
+	n_sample = int(1e4) if config.debug else int(1e5)
 
 	all_ja = [
 		polynomial.HermiteApproximation (),
@@ -129,13 +139,18 @@ def plot_cops_comp (debug=False):
 	plt.xlabel("Quantile q")
 	plt.ylabel("L2 Error")
 	plt.title("N="+str(N))
-	plt.show()
+	show_or_plot(path.join("chaos_expansions", "cops_error_N"+str(N)), config)
 
-def plot_error_ratio (debug=False):
-	N = 10 if debug else 40
+def plot_all_cops_comp (config):
+	all_N = [10] if config.debug else [20, 30, 40, 50]
+	for N in all_N:
+		plot_cops_comp(N, config)
+
+
+def plot_error_ratio (N, config):
 	all_q = np.linspace(0.05, 0.95, 20)
 
-	n_sample = int(1e4) if debug else int(1e5)
+	n_sample = int(1e4) if config.debug else int(1e5)
 	
 	all_ja = [
 		polynomial.HermiteApproximation (),
@@ -153,13 +168,18 @@ def plot_error_ratio (debug=False):
 	plt.xlabel("Quantile q")
 	plt.ylabel("Ratio of L2 error and error estimate")
 	plt.title("N="+str(N))
-	plt.show()
+	show_or_plot(path.join("chaos_expansions", "error_ratio_N"+str(N)), config)
 
-def plot_small_quantile (debug=False):
-	N = 10 if debug else 50
+def plot_all_error_ratio (config):
+	all_N = [10] if config.debug else [20, 30, 40, 50]
+	for N in all_N:
+		plot_error_ratio(N, config)
+
+def plot_small_quantile (config):
+	N = 10 if config.debug else 50
 	all_q = np.linspace(1e-3, 1e-4, 20)
 
-	n_sample = int(1e4) if debug else int(1e5)
+	n_sample = int(1e4) if config.debug else int(1e5)
 
 	all_ja = [
 		polynomial.HermiteApproximation (),
@@ -175,13 +195,13 @@ def plot_small_quantile (debug=False):
 	plt.xlabel("Quantile q")
 	plt.ylabel("L2 Error")
 	plt.title("N="+str(N))
-	plt.show()
+	show_or_plot(path.join("chaos_expansions", "small_quantile"), config)
 
-def plot_high_quantile (debug=False):
-	N = 10 if debug else 50
+def plot_high_quantile (config):
+	N = 10 if config.debug else 50
 	all_q = np.linspace(1-1e-3, 1-1e-4, 20)
 
-	n_sample = int(1e4) if debug else int(1e5)
+	n_sample = int(1e4) if config.debug else int(1e5)
 
 	all_ja = [
 		polynomial.HermiteApproximation (),
@@ -197,7 +217,7 @@ def plot_high_quantile (debug=False):
 	plt.xlabel("Quantile q")
 	plt.ylabel("L2 Error")
 	plt.title("N="+str(N))
-	plt.show()
+	show_or_plot(path.join("chaos_expansions", "high_quantile"), config)
 
 def plot_test (debug=False):
 	N = 10 if debug else 50
@@ -224,17 +244,18 @@ def plot_test (debug=False):
 
 def main ():
 	# print_coefs()
-	plot_all_l2 ()
+	config = Config(debug=False, save_plots=True, show_plots=False)
 
-	debug = False
+	plot_all_l2 (config)
+
 	
-	# plot_jacobi_comp(debug=debug)
-	# plot_cops_comp (debug=debug)
+	plot_jacobi_comp(config)
+	plot_all_cops_comp (config)
 
-	# plot_error_ratio (debug=debug)
+	plot_all_error_ratio (config)
 
-	# plot_small_quantile (debug=debug)
-	# plot_high_quantile (debug=debug)
+	plot_small_quantile (config)
+	plot_high_quantile (config)
 
 	# plot_test (debug=debug)
 
